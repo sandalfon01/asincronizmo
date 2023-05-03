5dda71f95660439ea6356cf83637bfdeb70fc034
sandalfon01 <sandalfon01@gmail.com>
2023-05-02 21:37:09 -0500

    usando asyn y await (￣m￣）

---
+++ src/async/index.js
@@ -0,0 +1,15 @@
+const fnAsync = ()=>{
+    return new Promise((resolve, reject)=>{
+        (true)
+            ? setTimeout(()=> resolve('Async!!'), 2000)
+            :reject(new Error('Error'));
+    });
+}
+const anotherFn= async()=>{
+    const somethig= await fnAsync();
+    console.log(somethig);
+    console.log('hello!');
+}
+console.log('before');
+anotherFn();
+console.log('After');
